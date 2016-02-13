7f55f1be7379723bf9fe9fa718e1d652dcffb45c
Jim Thompson <jimthompson5802@aol.com>
2016-02-13 18:42:22 -0500

    added function to record model performance

--- src/CommonFunctions.R
+++ src/CommonFunctions.R
@@ -34,3 +34,43 @@ caretLogLossSummary <- function(data,lev,model) {
     names(out) <- "LogLoss"
     out
 }
+
+# function to record model performance
+recordModelPerf <- function(model.performance.file,
+                            model=NULL,time.data=NULL,train.df=NULL,score=NA,
+                            improved="No", bestTune="", tune.grid="",
+                            model.parms="", comment="") {
+    # time.data is a proc_time object from system.time() function call
+    # bestTune is data frame for optimal model hyper-paramters
+    # 
+    
+    new.row <- data.frame(date.time=as.character(Sys.time()),
+                          model=model,
+                          user.cpu.time=summary(time.data)["user"],
+                          sys.cpu.time=summary(time.data)["system"],
+                          elapsed.time=summary(time.data)["elapsed"],
+                          num.observations=nrow(train.df),
+                          num.features=ncol(train.df),
+                          score=score,
+                          improved=improved,
+                          bestTune=bestTune,
+                          tune.grid=tune.grid,
+                          model.parms=model.parms,
+                          comment=comment,
+                          stringsAsFactors=FALSE) 
+    
+    # write new performance records
+    write.table(new.row,file=model.performance.file,
+                append=TRUE,sep="\t",row.names=FALSE,col.names=FALSE)
+    
+    return(NULL)
+}
+
+#function to flatten one or more rows of data.frame into a string representation
+flattenDF <- function(df) {
+    #convert data frame to list
+    x <- as.list(df)
+    
+    # convert named list to a string
+    return(paste(names(x),as.character(x),sep="=",collapse=","))
+}
