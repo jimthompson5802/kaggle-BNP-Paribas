2b410ab0c1c20b3d050c921698c6e086f88c4e7c
Jim Thompson <jimthompson5802@aol.com>
2016-02-14 16:47:27 -0500

    converted to faster read of input data

--- munge/create_train_calib_test.R
+++ munge/create_train_calib_test.R
@@ -9,7 +9,7 @@ library(data.table)
 data.dir <- "./data"
 
 
-raw <- data.table(read.csv(paste0(data.dir,"/train.csv"),stringsAsFactors=FALSE))
+raw <- fread(paste0(data.dir,"/train.csv"))
 setkey(raw,ID)
 
 nrow(raw)
@@ -18,19 +18,19 @@ table(raw$target)/nrow(raw)
 # create the partitions
 set.seed(13)
 idx <- createDataPartition(raw$target, p = 0.8, list=FALSE)
-train.raw <- raw[as.vector(idx)]
-raw <- raw[as.vector(-idx)]
+train.raw <- raw[idx]
+raw <- raw[-idx]
 
 set.seed(123)
 idx <- createDataPartition(raw$target, p = 0.5, list=FALSE)
 
-calib.raw <- raw[as.vector(idx)]
-test.raw <- raw[as.vector(-idx)]
+calib.raw <- raw[idx]
+test.raw <- raw[-idx]
 
 set.seed(17)
 idx <- createDataPartition(train.raw$target, p=0.5, list=FALSE)
-train0.raw <- train.raw[as.vector(idx)]
-train1.raw <- train.raw[as.vector(-idx)]
+train0.raw <- train.raw[idx]
+train1.raw <- train.raw[-idx]
 
 nrow(do.call(rbind,list(train0.raw,train1.raw,calib.raw,test.raw)))
 
@@ -43,3 +43,4 @@ rm(raw,train.raw)
 
 #save training, calibration and test data sets
 save(train0.raw,train1.raw,calib.raw,test.raw,file=paste0(data.dir,"/train_calib_test.RData"))
+
