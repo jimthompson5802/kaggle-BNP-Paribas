0a3baf09b293df3f99ad1a98f01c15f0ea90e766
Jim Thompson <jimthompson5802@aol.com>
2016-02-14 08:28:06 -0500

    standardized on level 0, level 1 nomenclature

--- munge/create_train_calib_test.R
+++ munge/create_train_calib_test.R
@@ -29,17 +29,17 @@ test.raw <- raw[as.vector(-idx)]
 
 set.seed(17)
 idx <- createDataPartition(train.raw$target, p=0.5, list=FALSE)
-train1.raw <- train.raw[as.vector(idx)]
-train2.raw <- train.raw[as.vector(-idx)]
+train0.raw <- train.raw[as.vector(idx)]
+train1.raw <- train.raw[as.vector(-idx)]
 
-nrow(do.call(rbind,list(train1.raw,train2.raw,calib.raw,test.raw)))
+nrow(do.call(rbind,list(train0.raw,train1.raw,calib.raw,test.raw)))
 
+table(train0.raw$target)/nrow(train0.raw)
 table(train1.raw$target)/nrow(train1.raw)
-table(train2.raw$target)/nrow(train2.raw)
 
 table(calib.raw$target)/nrow(calib.raw)
 table(test.raw$target)/nrow(test.raw)
 rm(raw,train.raw)
 
 #save training, calibration and test data sets
-save(train1.raw,train2.raw,calib.raw,test.raw,file=paste0(data.dir,"/train_calib_test.RData"))
+save(train0.raw,train1.raw,calib.raw,test.raw,file=paste0(data.dir,"/train_calib_test.RData"))
