bffe531cfaa18b6fca5db07277fd63bc3f9b03de
Jim Thompson <jimthompson5802@aol.com>
2016-02-13 23:16:49 -0500

    initial set up

--- src/skeleton_model/create_submission.R
+++ src/skeleton_model/create_submission.R
@@ -7,29 +7,31 @@ library(caret)
 library(caTools)
 
 # set working directory
-WORK.DIR <- "./src/logitboost_model"   # directory where model artifacts are stored
+WORK.DIR <- "./src/skeleton_model"   # directory where model artifacts are stored
 
 # Common Functions and Global variables
 source("./src/CommonFunctions.R")
 source(paste0(WORK.DIR,"/ModelCommonFunctions.R"))
 
+PREPARE.MODEL.DATA <- prepGBMModelData
+
 # read kaggle submission data
-new.df <- read.csv(unz(paste0(DATA.DIR,"/test.csv.zip"),"test.csv"),stringsAsFactors=FALSE)
+new.df <- read.csv(paste0(DATA.DIR,"/test.csv"),stringsAsFactors=FALSE)
 
 #save id vector
-id <- new.df$id
+id <- new.df$ID
 
 # prep the data for submission
-submission <- prepModelData(new.df,only.predictors=TRUE)
+submission <- PREPARE.MODEL.DATA(new.df,only.predictors=TRUE)
 
 # retrive generated model-name created in training run
-load(paste0(WORK.DIR,"/model_LogitBoost_2015-04-16_13_04_33.RData"))
+load(Sys.readlink(paste0(WORK.DIR,"/this_model.RData")))
 
 # predict class probabilities
 pred.probs <- predict(mdl.fit,newdata = submission$predictors,type = "prob")
 
 #create kaggle submission file
-write.csv(data.frame(id,pred.probs),file=paste0(WORK.DIR,"/submission.csv"),
+write.csv(data.frame(ID=id,PredictedProb=pred.probs[,"Yes"]),file=paste0(WORK.DIR,"/submission.csv"),
           row.names=FALSE)
 
 
