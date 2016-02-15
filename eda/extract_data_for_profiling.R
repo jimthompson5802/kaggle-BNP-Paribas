f02b34bbe28cf6ee7ab9a533eb2c4ede0cec1f98
Jim Thompson <jimthompson5802@aol.com>
2016-02-15 11:17:31 -0500

    added code to find numeric data with missing values separate saving sample.df and attr.data.types

--- eda/extract_data_for_profiling.R
+++ eda/extract_data_for_profiling.R
@@ -21,13 +21,20 @@ rm(train0.raw,train1.raw,test.raw,calib.raw,train.raw)
 
 # deterimine data types
 explanatory.attributes <- setdiff(names(sample.df),c(ID.VAR,TARGET.VAR))
-data.classes <- sapply(explanatory.attributes,function(x){class(sample.df[,x])})
+data.classes <- sapply(explanatory.attributes,function(x){class(sample.df[[x]])})
 
 # categorize data types in the data set?
 unique.classes <- unique(data.classes)
 
-attr.by.data.types <- lapply(unique.classes,function(x){names(data.classes[data.classes==x])})
-names(attr.by.data.types) <- unique.classes
-comment(attr.by.data.types) <- "list that categorize training data types"
-save(sample.df,attr.by.data.types,
-     file=paste0(DATA.DIR,"/sample_data_for_eda.RData"))
+attr.data.types <- lapply(unique.classes,function(x){names(data.classes[data.classes==x])})
+names(attr.data.types) <- unique.classes
+comment(attr.data.types) <- "list that categorize training data types"
+
+# find numeric attributes that contain missing values
+num.missing <- sapply(c(attr.data.types$numeric,attr.data.types$integer),
+                      function(x){sum(is.na(sample.df[[x]]))})
+attr.data.types$contain.na <- names(num.missing)[num.missing>0]
+
+
+save(sample.df,file=paste0(DATA.DIR,"/sample_data_for_eda.RData"))
+save(attr.data.types,file=paste0(DATA.DIR,"/attr_data_types.RData"))
