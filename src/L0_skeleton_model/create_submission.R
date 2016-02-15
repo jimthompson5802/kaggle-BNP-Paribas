cd4ff0aff65d32d83a99a677e84f2317e07e7f9e
Jim Thompson <jimthompson5802@aol.com>
2016-02-15 17:54:25 -0500

    Modified to pickup data prep function from saved model structure Corrected selection on Class_1 probability

--- src/L0_skeleton_model/create_submission.R
+++ src/L0_skeleton_model/create_submission.R
@@ -13,7 +13,8 @@ WORK.DIR <- "./src/L0_skeleton_model"   # directory where model artifacts are st
 # Common Functions and Global variables
 source("./src/CommonFunctions.R")
 
-PREPARE.MODEL.DATA <- prepL0SkltnModelData
+# retrive generated model-name created in training run
+load(Sys.readlink(paste0(WORK.DIR,"/this_model.RData")))
 
 # read kaggle submission data
 new.df <- fread(paste0(DATA.DIR,"/test.csv"))
@@ -24,14 +25,11 @@ id <- new.df$ID
 # prep the data for submission
 submission <- PREPARE.MODEL.DATA(new.df,includeResponse=FALSE)
 
-# retrive generated model-name created in training run
-load(Sys.readlink(paste0(WORK.DIR,"/this_model.RData")))
-
 # predict class probabilities
 pred.probs <- predict(mdl.fit,newdata = submission$predictors,type = "prob")
 
 #create kaggle submission file
-write.csv(data.frame(ID=id,PredictedProb=pred.probs[,"Yes"]),file=paste0(WORK.DIR,"/submission.csv"),
+write.csv(data.frame(ID=id,PredictedProb=pred.probs[,"Class_1"]),file=paste0(WORK.DIR,"/submission.csv"),
           row.names=FALSE)
 
 
