df790b7181d83ec4e9fc0ccee2c82e5acbf3e701
Jim Thompson <jimthompson5802@aol.com>
2016-02-18 07:15:10 -0500

    retrained model using train0 and train1 data sets

--- src/L0_gbm1/train_model.R
+++ src/L0_gbm1/train_model.R
@@ -47,7 +47,7 @@ FRACTION.TRAIN.DATA <- 1.0
 
 # get training data
 load(paste0(DATA.DIR,"/train_calib_test.RData"))
-train.df <- train0.raw
+train.df <- rbind(train0.raw,train1.raw)
 
 # extract subset for inital training
 set.seed(29)
