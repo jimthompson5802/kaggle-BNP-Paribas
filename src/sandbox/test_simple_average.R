43b9abc0e6f562a0d25aed5c9c5538cab4ea7e9b
Jim Thompson <jimthompson5802@aol.com>
2016-02-28 09:57:28 -0500

    added saving predicted probabilities

--- src/sandbox/test_simple_average.R
+++ src/sandbox/test_simple_average.R
@@ -4,3 +4,14 @@
 
 l1_gbm2 <- read.csv("./src/L1_gbm2/submission.csv")
 l1_nnet1 <- read.csv("./src/L1_nnet1/submission.csv")
+
+
+cor(l1_gbm2[,"PredictedProb"],l1_nnet1[,"PredictedProb"])
+
+PredictedProb <- apply(cbind(l1_gbm2[,"PredictedProb"],l1_nnet1[,"PredictedProb"]),1,sum)/2
+
+
+write.csv(cbind(ID=l1_gbm2[,"ID"],PredictedProb),
+          file="./src/sandbox/average_test_submission.csv",
+          row.names = FALSE)
+
