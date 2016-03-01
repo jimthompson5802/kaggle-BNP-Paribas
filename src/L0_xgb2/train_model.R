ee689847e6a17e4aafc54696ae1505ba66728215
Jim Thompson <jimthompson5802@aol.com>
2016-02-29 21:28:58 -0500

    trained with train0 and train1

--- src/L0_xgb2/train_model.R
+++ src/L0_xgb2/train_model.R
@@ -46,14 +46,14 @@ MODEL.SPECIFIC.PARMS <- list(verbose=0)
 PREPARE.MODEL.DATA <- function(data){return(data)}  #default data prep
 PREPARE.MODEL.DATA <- prepL0FeatureSetAll
 
-MODEL.COMMENT <- "custom xgb tree model"
+MODEL.COMMENT <- "custom xgb tree model, trian0 and train1"
 
 # amount of data to train
 FRACTION.TRAIN.DATA <- 1.0
 
 # get training data
 load(paste0(DATA.DIR,"/train_calib_test.RData"))
-train.df <- train0.raw
+train.df <- rbind(train0.raw,train1.raw)
 
 # extract subset for inital training
 set.seed(29)
