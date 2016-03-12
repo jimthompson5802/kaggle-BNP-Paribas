7515235210fa2bfdf5e08d112fc53f8facb83d74
Jim Thompson <jimthompson5802@aol.com>
2016-03-12 08:02:28 -0500

    reset to train0 only on experimental branch

--- src/L0_xgb3/train_model.R
+++ src/L0_xgb3/train_model.R
@@ -46,10 +46,13 @@ MODEL.SPECIFIC.PARMS <- list(verbose=0)
 PREPARE.MODEL.DATA <- function(data){return(data)}  #default data prep
 PREPARE.MODEL.DATA <- prepL0FeatureSet2a
 
-MODEL.COMMENT <- "custom xgb tree model, with train0.raw"
+MODEL.COMMENT <- "custom xgb tree model, reset to train0.raw"
 
 # amount of data to train
 FRACTION.TRAIN.DATA <- 1.0
+
+# force recording model flag
+FORCE_RECORDING_MODEL <- TRUE
 
 # get training data
 load(paste0(DATA.DIR,"/train_calib_test.RData"))
@@ -120,7 +123,7 @@ tail(modelPerf.df[,1:10],1)
 
 # if last score recorded is better than previous ones save model object
 last.idx <- length(modelPerf.df$score)
-if (last.idx == 1 || improved == "Yes") {
+if (last.idx == 1 || improved == "Yes" || FORCE_RECORDING_MODEL) {
     cat("found improved model, saving...\n")
     flush.console()
     #yes we have improvement or first score, save generated model
