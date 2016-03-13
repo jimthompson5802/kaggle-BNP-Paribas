ae7aa411a5f50da0c3efc964db876500c1d1d70d
Jim Thompson <jimthompson5802@aol.com>
2016-03-13 00:15:43 -0500

    migrated from experimental branch

--- src/L2_blend/blend_models.R
+++ src/L2_blend/blend_models.R
@@ -13,21 +13,39 @@ WORK.DIR <- "./src/L2_blend"
 
 MODEL.METHOD <- "blend"
 
-load(paste0(DATA.DIR,"/train_calib_test.RData"))
+MODEL.COMMENT <- "Determine optimal weights"
 
-# training data for blending
-train.data <- prepL2FeatureSet(calib.raw)
+FORCE_RECORDING_MODEL <- FALSE
 
+# get training data for calibration
+# L1_gbm2
+load("./src/L1_gbm2/data_for_level2_optimization.RData")
+calib.gbm2 <- calib.pred.probs
 
+# L1_nnet1
+load("./src/L1_nnet1/data_for_level2_optimization.RData")
+calib.nnet1 <- calib.pred.probs
 
-#
-#  simple average
-#
-avg.probs <- apply(train.data$predictors,1,sum)/ncol(train.data$predictors)
+# combine Level 1 Calibration data
+train.data <- list()
+train.data$predictors <- cbind(gbm2=calib.gbm2[,"Class_1"],
+                               nnet1=calib.nnet1[,"Class_1"])
 
-cat("simple avg",logLossEval(avg.probs,train.data$response),"\n")
+train.data$response = calib.gbm2$target
 
+# get test data for calibration
+# L1_gbm2
+test.gbm2 <- test.pred.probs
 
+# L1_nnet1
+test.nnet1 <- test.pred.probs
+
+# combine Level 1 Calibration data
+test.data <- list()
+test.data$predictors <- cbind(gbm2=test.gbm2[,"Class_1"],
+                               nnet1=test.nnet1[,"Class_1"])
+
+test.data$response = test.gbm2$target
 
 
 #
@@ -39,11 +57,15 @@ makeBlendFunction <- function(target,probs.mat) {
     
     function(w) {
         
-        pred.probs <- probs.mat * matrix(rep(w,nrow(probs.mat)),
-                                             ncol=ncol(probs.mat),byrow=TRUE)
+        wt.mat <- matrix(rep(w,nrow(probs.mat)),
+                         ncol=ncol(probs.mat),byrow=TRUE)
+        
+        pred.probs <- probs.mat * wt.mat
+            
         pred.probs <- apply(pred.probs,1,sum)
         
-        logLossEval(pred.probs,target)
+        ans <- logLossEval(pred.probs,target)
+        return(ans)
     }
 }
 
@@ -59,7 +81,7 @@ heq <- function(w) {
 
     return(h)
 }
-
+# 
 # heq.jac <- function(w){
 #     j <- matrix(NA,1,length(w))
 #     j[1,] <- rep(1,length(w))
@@ -78,64 +100,73 @@ hin <- function(w) {
 #     return(rbind(diag(1,27),diag(-1,27)))
 # }
 # 
-system.time(opt.wts <- constrOptim.nl(rep(1/ncol(train.data$predictors),ncol(train.data$predictors)),
+time.data <- system.time(opt.wts <- constrOptim.nl(rep(1/ncol(train.data$predictors),ncol(train.data$predictors)),
                                       fn=ensFunc,  #gr=grFunc,
                                       hin=hin, #hin.jac=hin.jac,
                                       heq=heq, #heq.jac=heq.jac,
                                       #control.outer=list(itmax=10),
                                       control.optim=list(trace=2)))
 
-score <- opt.wts$value
-
-names(opt.wts$par) <- names(train.data$predictors)
-cat("optimaal weights",opt.wts$par,", training score:",opt.wts$value,"\n")
+names(opt.wts$par) <- colnames(train.data$predictors)
+cat("optimal weights",opt.wts$par,", training score:",opt.wts$value,"\n")
 
 
 # check score for test data
-test.data <- prepL2FeatureSet(test.raw)
-
 pred.probs <- test.data$predictors * matrix(rep(opt.wts$par,nrow(test.data$predictors)),
                                             ncol=ncol(test.data$predictors),byrow=TRUE)
 pred.probs <- apply(pred.probs,1,sum)
 
-test.score <- logLossEval(pred.probs,test.data$response)
+score <- logLossEval(pred.probs,test.data$response)
 
-cat("optimaal weights",opt.wts$par,", test score:",test.score,"\n")
+cat("optimal weights",opt.wts$par,", test score:",score,"\n")
 
 #
 # record Model performance
 #
 
-ensemble.weights <- c(opt.wts$par[1],opt.wts$par[2], opt.wts$par[3])
-names(ensemble.weights) <- c("rf", "gbm_one_vs_all","gbm_one_vs_all_synth")
+blending.weights <- opt.wts$par
 
-model.weights <- paste(c("rf", "gbm_one_vs_all","gbm_one_vs_all_syth"),ensemble.weights,sep="=",collapse=",")
+model.weights <- paste(names(blending.weights),blending.weights,sep="=",collapse=",")
 bestTune <- data.frame(model.weights, stringsAsFactors=FALSE)
 
-# set up dummy data structures to account for recrodModelPerf() function
-time.data <- system.time(dummy.df <- data.frame())
+# record Model performance
+modelPerf.df <- read.delim(paste0(WORK.DIR,"/model_performance.tsv"),
+                           stringsAsFactors=FALSE)
+# determine if score improved
+improved <- ifelse(score < min(modelPerf.df$score),"Yes","No")
 
-# record performance
-modPerf.df <- recordModelPerf(modPerf.df,MODEL.METHOD,time.data,
-                              dummy.df,
-                              score,bestTune)
-save(modPerf.df,file=paste0(WORK.DIR,"/modPerf.RData"))
+recordModelPerf(paste0(WORK.DIR,"/model_performance.tsv"),
+                MODEL.METHOD,
+                time.data,
+                train.data$predictors,
+                score,
+                improved=improved,
+                bestTune=flattenDF(bestTune),
+                tune.grid=NA,
+                model.parms=NA,
+                comment=MODEL.COMMENT)
+
+modelPerf.df <- read.delim(paste0(WORK.DIR,"/model_performance.tsv"),
+                           stringsAsFactors=FALSE)
+
 
 #display model performance record for this run
-tail(modPerf.df[,1:(ncol(modPerf.df)-1)],1)
+tail(modelPerf.df[,1:10],1)
 
 # if last score recorded is better than previous ones save model object
-last.idx <- length(modPerf.df$score)
-if (last.idx == 1 ||
-        modPerf.df$score[last.idx] < min(modPerf.df$score[1:(last.idx-1)])) {
+last.idx <- length(modelPerf.df$score)
+if (last.idx == 1 || improved == "Yes" || FORCE_RECORDING_MODEL) {
     cat("found improved model, saving...\n")
     flush.console()
     #yes we have improvement or first score, save generated model
-    file.name <- paste0("/ensembleWeights_",Sys.time(),".RData")
+    file.name <- paste0("model_",MODEL.METHOD,"_",modelPerf.df$date.time[last.idx],".RData")
     file.name <- gsub(" ","_",file.name)
     file.name <- gsub(":","_",file.name)
     
-    save(ensemble.weights,file=paste0(WORK.DIR,file.name))
+    save(blending.weights,file=paste0(WORK.DIR,"/",file.name))
+    
+    # estalish pointer to current model
+    writeLines(file.name,paste0(WORK.DIR,"/this_model"))
 } else {
     cat("no improvement!!!\n")
     flush.console()
