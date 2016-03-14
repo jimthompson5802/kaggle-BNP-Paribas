03ec664034aa2592ef0366189c6a31776495db8c
Jim Thompson <jimthompson5802@aol.com>
2016-03-13 21:52:20 -0400

    improved model with all train0

--- src/L0_rngr2/train_model.R
+++ src/L0_rngr2/train_model.R
@@ -43,7 +43,7 @@ PREPARE.MODEL.DATA <- prepL0FeatureSetAll
 MODEL.COMMENT <- "expanded boruta feature set, imputing median values"
 
 # amount of data to train
-FRACTION.TRAIN.DATA <- 0.25
+FRACTION.TRAIN.DATA <- 1.0
 
 # get training data
 load(paste0(DATA.DIR,"/train_calib_test.RData"))
