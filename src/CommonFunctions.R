6156823efb0091d40ffc7701bb1aaca08dc05ff8
Jim Thompson <jimthompson5802@aol.com>
2016-03-18 14:14:49 -0400

    create feature set

--- src/CommonFunctions.R
+++ src/CommonFunctions.R
@@ -531,7 +531,7 @@ prepL0FeatureSetAll <- function(df,includeResponse=TRUE){
     return(ans)
 }
 
-# Feature set based on Rscipt
+# Feature set based on Rscipt w/ one synthetic feature
 # Categorical represented as integers
 # numeric NA set to -999
 prepL0FeatureSet3 <- function(df,includeResponse=TRUE){
@@ -553,6 +553,59 @@ prepL0FeatureSet3 <- function(df,includeResponse=TRUE){
     
     # eliminate unwanted variables
     predictors <- cbind(df[,predictor.vars,with=FALSE],all.var.na.count)
+    
+    # get data types and change all strings to factors
+    load(paste0(DATA.DIR,"/attr_data_types.RData"))
+    load(paste0(DATA.DIR,"/factor_levels.RData"))
+    
+    # set NA to -999
+    num.attr <- intersect(predictor.vars,c(attr.data.types$numeric,attr.data.types$integer))
+    for (x in num.attr) {
+        idx <- is.na(predictors[[x]])
+        predictors[[x]][idx] <- -999
+    }
+    
+    # standardize factor levels for modeling
+    char.attr <- intersect(predictor.vars,attr.data.types$character)
+    
+    for (x in char.attr) {
+        predictors[[x]] <- as.integer(factor(predictors[[x]],levels = factor.levels[[x]]))
+    }
+    
+    
+    if (includeResponse) {
+        
+        response <- factor(ifelse(df$target == 1,"Class_1","Class_0"),
+                           levels=c("Class_1","Class_0"))
+        ans <- list(predictors=predictors,response=response)
+        
+    } else {
+        
+        ans <- list(predictors=predictors)
+    }
+    
+    return(ans)
+}
+
+# Feature set based on Rscipt
+# Categorical represented as integers
+# numeric NA set to -999
+prepL0FeatureSet4 <- function(df,includeResponse=TRUE){
+    # prepL0FeatureSet4
+    # df: raw data
+    # if only.predcitors is TRUE then return list(predictors)
+    # if only.predictors is FALSE then return list(predictors,response)
+    
+    require(plyr)
+    require(caret)
+    
+    predictor.vars <- setdiff(names(df),c('ID','target','v8','v23','v25','v31','v36','v37','v46','v51',
+                                          'v53','v54','v63','v73','v75','v79','v81','v82','v89','v92',
+                                          'v95','v105','v107','v108','v109','v110','v116','v117','v118',
+                                          'v119','v123','v124','v128'))
+    
+    # eliminate unwanted variables
+    predictors <- df[,predictor.vars,with=FALSE]
     
     # get data types and change all strings to factors
     load(paste0(DATA.DIR,"/attr_data_types.RData"))
