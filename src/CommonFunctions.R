69e60d111ce89b60c5577a5142ad4f120209418c
Jim Thompson <jimthompson5802@aol.com>
2016-03-27 11:41:02 -0400

    added prepL0FeatureSet5 - categorical variables set to dummy variable representation

--- src/CommonFunctions.R
+++ src/CommonFunctions.R
@@ -645,6 +645,62 @@ prepL0FeatureSet4 <- function(df,includeResponse=TRUE){
     return(ans)
 }
 
+# Features selected from expanded Boruta analysis 
+# number attributes set to raw values, NA set to -999
+# character attributes set as dummy variables
+prepL0FeatureSet5 <- function(df,includeResponse=TRUE){
+    # prepL0FeatureSet5
+    # df: raw data
+    # if only.predcitors is TRUE then return list(predictors)
+    # if only.predictors is FALSE then return list(predictors,response)
+    
+    require(plyr)
+    require(caret)
+    require(Boruta)
+    
+    # use only attributes confirmed by Boruta feature analysis
+    load(paste0(DATA.DIR,"/boruta_feature_analysis2.RData"))
+    
+    # get data types and change all strings to factors
+    load(paste0(DATA.DIR,"/attr_data_types.RData"))
+    load(paste0(DATA.DIR,"/factor_levels.RData"))
+    
+    number.vars <- union(intersect(getSelectedAttributes(bor.results),attr.data.types$numeric),
+                         intersect(getSelectedAttributes(bor.results),attr.data.types$integer))
+    
+    # get categorical variables
+    char.vars <- intersect(getSelectedAttributes(bor.results),attr.data.types$character)
+    
+    # create dummy variables 
+    ll <- lapply(char.vars,function(x){
+        y <- predict(dummy.vars[[x]],newdata=df[,x,with=FALSE])
+        return(y)
+    })
+    
+    y <- do.call(cbind,ll)
+    
+    predictors <- cbind(df[,number.vars,with=FALSE],y)
+    
+    # set NA to -999
+    for (x in number.vars) {
+        idx <- is.na(predictors[[x]])
+        predictors[[x]][idx] <- -999
+    }
+    
+    if (includeResponse) {
+        
+        response <- factor(ifelse(df$target == 1,"Class_1","Class_0"),
+                           levels=c("Class_1","Class_0"))
+        ans <- list(predictors=predictors,response=response)
+        
+    } else {
+        
+        ans <- list(predictors=predictors)
+    }
+    
+    return(ans)
+}
+
 # data prep Level 0 rngr1 model
 prepL0rngr1ModelData <- function(df,includeResponse=TRUE){
     # df: raw data
