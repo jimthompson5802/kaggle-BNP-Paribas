df094e5b1b936c0b1ef3eb3ffdceb4c54fa48f96
Jim Thompson <jimthompson5802@aol.com>
2016-03-27 14:07:32 -0400

    added L0_gbm4, no increase in model performance

--- src/L1_nnet1/train_model.R
+++ src/L1_nnet1/train_model.R
@@ -43,7 +43,7 @@ PREPARE.MODEL.DATA <- prepL1FeatureSet1
 MODEL.COMMENT <- "Only Class_1 probabilites as features"
 
 LEVEL0.MODELS <- c("L0_gbm2",
-                   #"L0_rngr1",
+                   "L0_gbm4",
                    "L0_xtc1",
                    "L0_xtc2",
                    "L0_xtc3",
