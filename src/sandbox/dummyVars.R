b41ae3680c58e7466bc99e1dc3fffaf85d837619
Jim Thompson <jimthompson5802@aol.com>
2016-03-27 11:38:08 -0400

    initial check-in and test

---
+++ src/sandbox/dummyVars.R
@@ -0,0 +1,67 @@
+###
+#  Dummy vars test
+###
+
+library(caret)
+
+source("./src/CommonFunctions.R")
+
+load(file=paste0(DATA.DIR,"/train_calib_test.RData"))
+
+# Features selected from expanded Boruta analysis 
+# character attributes set as dummy variables
+prepL0FeatureSet5 <- function(df,includeResponse=TRUE){
+    # prepL0FeatureSet5
+    # df: raw data
+    # if only.predcitors is TRUE then return list(predictors)
+    # if only.predictors is FALSE then return list(predictors,response)
+    
+    require(plyr)
+    require(caret)
+    require(Boruta)
+    
+    # use only attributes confirmed by Boruta feature analysis
+    load(paste0(DATA.DIR,"/boruta_feature_analysis2.RData"))
+    
+    # get data types and change all strings to factors
+    load(paste0(DATA.DIR,"/attr_data_types.RData"))
+    load(paste0(DATA.DIR,"/factor_levels.RData"))
+    
+    number.vars <- union(intersect(getSelectedAttributes(bor.results),attr.data.types$numeric),
+          intersect(getSelectedAttributes(bor.results),attr.data.types$integer))
+    
+    # get categorical variables
+    char.vars <- intersect(getSelectedAttributes(bor.results),attr.data.types$character)
+    
+    # create dummy variables 
+    ll <- lapply(char.vars,function(x){
+        y <- predict(dummy.vars[[x]],newdata=df[,x,with=FALSE])
+        return(y)
+    })
+    
+    y <- do.call(cbind,ll)
+    
+    predictors <- cbind(df[,number.vars,with=FALSE],y)
+    
+    # set NA to -999
+    for (x in number.vars) {
+        idx <- is.na(predictors[[x]])
+        predictors[[x]][idx] <- -999
+    }
+    
+    if (includeResponse) {
+        
+        response <- factor(ifelse(df$target == 1,"Class_1","Class_0"),
+                           levels=c("Class_1","Class_0"))
+        ans <- list(predictors=predictors,response=response)
+        
+    } else {
+        
+        ans <- list(predictors=predictors)
+    }
+    
+    return(ans)
+}
+
+
+train.data <- prepL0FeatureSet5(train0.raw)
