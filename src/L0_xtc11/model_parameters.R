769330de9eb82fc62f6b2587eb312cb23ec3271e
Jim Thompson <jimthompson5802@aol.com>
2016-04-12 17:42:09 -0400

    clean out R-specific parameters since this is a Python model

--- src/L0_xtc11/model_parameters.R
+++ src/L0_xtc11/model_parameters.R
@@ -3,16 +3,5 @@
 ###
 
 
-# set caret training parameters
-CARET.TRAIN.PARMS <- list(method="xgbTree")   # Replace MODEL.METHOD with appropriate caret model
+PREPARE.MODEL.DATA <- prepL0FeatureSetAll
 
-CARET.TUNE.GRID <-  expand.grid(nrounds=500, 
-                                max_depth=10, 
-                                eta=0.01, 
-                                gamma=0.1, 
-                                colsample_bytree=0.8, 
-                                min_child_weight=1)
-
-PREPARE.MODEL.DATA <- prepL0FeatureSet2a
-
-MODEL.SPECIFIC.PARMS <- list(verbose=0) #NULL # Other model specific parameters
