2d017333706f963f20fabd0e5f61151207287a98
Jim Thompson <jimthompson5802@aol.com>
2016-04-15 08:00:27 -0400

    added ploting and tracking tuning of hyper-parameters

--- src/L1_xgb11/custom_tune_model.R
+++ src/L1_xgb11/custom_tune_model.R
@@ -5,7 +5,8 @@
 library(data.table)
 library(caret)
 # add any model specific package library commands
-
+library(xgboost)
+library(ggplot2)
 
 # set working directory
 WORK.DIR <- "./src/L1_xgb11"  # modify to specify directory to contain model artififacts
@@ -45,29 +46,46 @@ registerDoMC(cores = 7)
 Sys.time()
 set.seed(825)
 
-param <- list(boster=gbtree,
-              max.depth=2,
-              eta=0.3,
+param <- list(booster="gbtree",
+              objecive="binary:logistic",
+              eval_metric="logloss",
+              max.depth=6,
+              eta=0.01,
               gamma=0,
               nthread=2, 
               min_child_weight=1,
-              subsample=1,
-              colsample_bytree=1,
-              nround=2)
+              subsample=0.8,
+              colsample_bytree=0.8,
+              seed=13)
 
-res <- xgb.cv(params=param, data=train.data$predictors, 
-              label=train.data$response,
-              nround=2,
+
+dTrain <- xgb.DMatrix(as.matrix(train.data$predictors),
+                      label=as.numeric(train.data$response=="Class_1"))
+set.seed(13)
+res <- xgb.cv(params=param, 
+              data=dTrain,
+              nrounds=2000,
+              nfold=5,
+              early.stop.round=30,
               prediction=TRUE)
 
 
-time.data
-mdl.fit
-# stopCluster(cl)
+best.round <- which(res$dt$test.logloss.mean == min(res$dt$test.logloss.mean))
 
-score <- mean(mdl.fit$resample$LogLoss)
-score
+tune.df <- data.frame(best.round=best.round,
+                      train.error = res$dt$train.logloss.mean[best.round],
+                      test.error = res$dt$test.logloss.mean[best.round],
+                      param)
+write.table(tune.df,file=paste0(WORK.DIR,"/hyper_parameter_tune_log.tsv"),
+            append=TRUE,sep="\t",row.names=FALSE,col.names=FALSE)
 
+# plot 
+p <- ggplot(res$dt) +
+    geom_line(aes(1:nrow(res$dt),res$dt$train.logloss.mean),color="blue") +
+    geom_line(aes(1:nrow(res$dt),res$dt$test.logloss.mean),color="red") +
+    ylab("LogLoss Erroor") +
+    xlab("nrounds")
+print(p)
 
 
 
