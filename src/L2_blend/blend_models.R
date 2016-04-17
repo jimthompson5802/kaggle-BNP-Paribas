00cf8956178c14e26d377d2bc9e3cb54b5b49633
Jim Thompson <jimthompson5802@aol.com>
2016-04-17 13:42:54 -0400

    updated

--- src/L2_blend/blend_models.R
+++ src/L2_blend/blend_models.R
@@ -18,6 +18,7 @@ MODEL.COMMENT <- "Determine optimal weights"
 FORCE_RECORDING_MODEL <- FALSE
 
 # get training data for calibration
+<<<<<<< HEAD
 # L1_gbm2
 load("./src/L1_gbm2/data_for_level2_optimization.RData")
 calib.gbm2 <- calib.pred.probs
@@ -46,6 +47,22 @@ test.data$predictors <- cbind(gbm2=test.gbm2[,"Class_1"],
                                nnet1=test.nnet1[,"Class_1"])
 
 test.data$response = test.gbm2$target
+=======
+# L1_nnet11
+nnet11.env <- new.env()
+load("./src/L1_nnet11/level2_features.RData",envir=nnet11.env)
+
+# L1_xgb11
+xgb11.env <- new.env()
+load("./src/L1_xgb11/level2_features.RData",envir=xgb11.env)
+
+# combine Level 1 Calibration data
+train.data <- list()
+train.data$predictors <- cbind(nnet11=nnet11.env$level2.data[,"Class_1"],
+                               xgb11=xgb11.env$level2.data[,"Class_1"])
+
+train.data$response = nnet11.env$level2.data$response
+>>>>>>> experimental
 
 
 #
@@ -81,11 +98,23 @@ heq <- function(w) {
 
     return(h)
 }
+<<<<<<< HEAD
 # 
 # heq.jac <- function(w){
 #     j <- matrix(NA,1,length(w))
 #     j[1,] <- rep(1,length(w))
 # }
+=======
+
+heq.jac <- function(w){
+    
+    j <- matrix(NA,1,length(w))
+    
+    j[1,] <- c(1,1)
+
+    return(j)
+}
+>>>>>>> experimental
 
 
 # define inequality constraints
@@ -94,6 +123,7 @@ hin <- function(w) {
     return(c(w,1-w))
     
 }
+<<<<<<< HEAD
 # 
 # hin.jac <- function(w) {
 #     
@@ -101,9 +131,18 @@ hin <- function(w) {
 # }
 # 
 time.data <- system.time(opt.wts <- constrOptim.nl(c(0.0001,0.9999),
+=======
+
+hin.jac <- function(w) {
+
+    return(rbind(diag(1,2),diag(-1,2)))
+}
+
+time.data <- system.time(opt.wts <- constrOptim.nl(c(0.5,0.5),
+>>>>>>> experimental
                                       fn=ensFunc,  #gr=grFunc,
-                                      hin=hin, #hin.jac=hin.jac,
-                                      heq=heq, #heq.jac=heq.jac,
+                                      hin=hin, hin.jac=hin.jac,
+                                      heq=heq, heq.jac=heq.jac,
                                       #control.outer=list(itmax=10),
                                       control.optim=list(trace=2)))
 
@@ -126,6 +165,7 @@ cat("optimal weights",opt.wts$par,", test score:",score,"\n")
 
 blending.weights <- opt.wts$par
 
+<<<<<<< HEAD
 model.weights <- paste(names(blending.weights),blending.weights,sep="=",collapse=",")
 bestTune <- data.frame(model.weights, stringsAsFactors=FALSE)
 
@@ -171,4 +211,51 @@ if (last.idx == 1 || improved == "Yes" || FORCE_RECORDING_MODEL) {
     cat("no improvement!!!\n")
     flush.console()
 }
+=======
+# model.weights <- paste(names(blending.weights),blending.weights,sep="=",collapse=",")
+# bestTune <- data.frame(model.weights, stringsAsFactors=FALSE)
+# 
+# # record Model performance
+# modelPerf.df <- read.delim(paste0(WORK.DIR,"/model_performance.tsv"),
+#                            stringsAsFactors=FALSE)
+# # determine if score improved
+# improved <- ifelse(score < min(modelPerf.df$score),"Yes","No")
+# 
+# recordModelPerf(paste0(WORK.DIR,"/model_performance.tsv"),
+#                 MODEL.METHOD,
+#                 time.data,
+#                 train.data$predictors,
+#                 score,
+#                 improved=improved,
+#                 bestTune=flattenDF(bestTune),
+#                 tune.grid=NA,
+#                 model.parms=NA,
+#                 comment=MODEL.COMMENT)
+# 
+# modelPerf.df <- read.delim(paste0(WORK.DIR,"/model_performance.tsv"),
+#                            stringsAsFactors=FALSE)
+# 
+# 
+# #display model performance record for this run
+# tail(modelPerf.df[,1:10],1)
+# 
+# # if last score recorded is better than previous ones save model object
+# last.idx <- length(modelPerf.df$score)
+# if (last.idx == 1 || improved == "Yes" || FORCE_RECORDING_MODEL) {
+#     cat("found improved model, saving...\n")
+#     flush.console()
+#     #yes we have improvement or first score, save generated model
+#     file.name <- paste0("model_",MODEL.METHOD,"_",modelPerf.df$date.time[last.idx],".RData")
+#     file.name <- gsub(" ","_",file.name)
+#     file.name <- gsub(":","_",file.name)
+#     
+#     save(blending.weights,file=paste0(WORK.DIR,"/",file.name))
+#     
+#     # estalish pointer to current model
+#     writeLines(file.name,paste0(WORK.DIR,"/this_model"))
+# } else {
+#     cat("no improvement!!!\n")
+#     flush.console()
+# }
+>>>>>>> experimental
 
