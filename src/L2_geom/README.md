986d7929d2ef3940efb3b70fa8b6738984d4c57e
Jim Thompson <jimthompson5802@aol.com>
2016-04-17 10:35:15 -0400

    initial check-in

---
+++ src/L2_geom/README.md
@@ -0,0 +1,5 @@
+Level 2 Geometric Mean of L1 models
+==================================================
+
+## Overview
+Calculates blending weights to combine the Level 1 models
---
+++ src/L2_geom/create_submission.R
@@ -0,0 +1,52 @@
+###
+#  create ensemble model combining selected models
+###
+
+library(data.table)
+library(caret)
+
+
+# import global variabels and common functions
+source("./src/CommonFunctions.R")
+WORK.DIR <- "./src/L2_geom"
+
+# retrieve Level 1 submissions
+# L1_xgb11
+xgb11.pred.probs <- read.csv("./src/L1_xgb11/submission.csv")
+
+#L1_nnet11
+nnet11.pred.probs <- read.csv("./src/L1_nnet11/submission.csv")
+
+#create data for predictions
+submission <- list()
+submission$predictors <- cbind(xgb11=xgb11.pred.probs[,"PredictedProb"],
+                               nnet11=nnet11.pred.probs[,"PredictedProb"])
+
+id <- nnet11.pred.probs$ID
+
+#
+# Average the individual probablities
+#
+
+pred.probs <- sqrt(submission$predictors[,"xgb11"] * submission$predictors[,"nnet11"])
+
+
+# # 
+# # use optimal weights calculated$
+# #
+# 
+# # combine three models probabilities into a single matrix
+# probs.mat <- cbind(rf2.probs,gbm2.probs,gbm4.probs)
+# 
+# #set up weight matrix to combine the individual weights
+# wmat <- rbind(diag(opt.wts$par[1:9]),diag(opt.wts$par[10:18]),diag(opt.wts$par[19:27]))
+# 
+# # compute overall probablities using invidual model class weights
+# pred.probs <- probs.mat %*% wmat
+# colnames(pred.probs) <- paste0("Class_",1:9)
+#     
+
+#create kaggle submission file
+write.csv(data.frame(ID=id,PredictedProb=pred.probs),file=paste0(WORK.DIR,"/submission.csv"),
+          row.names=FALSE)
+
